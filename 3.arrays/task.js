ce437a4397b0b5a8c5375ef3148618bb139db6d3
Denis Borodkin <den_borodkin@inbox.ru>
2022-08-24 20:53:45 +0300

    Home_Work_Array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
-  let result;
+	let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);;
 
-  // Ваш код
 
-  return result; // boolean
+
+
+	return result;
+
+
+
 }
+compareArrays(arr1, arr2)
 
 function advancedFilter(arr) {
-  let resultArr;
+	let resultArr = arr.filter(number => number > 0 && number % 3 === 0).map(number => number * 10);
 
-  // Ваш код
 
-  return resultArr; // array
+
+	return resultArr;
 }
