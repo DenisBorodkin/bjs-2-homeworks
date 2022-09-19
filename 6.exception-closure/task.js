756a5c84cfa50459023e0157b37884617706efe3
Denis Borodkin <den_borodkin@inbox.ru>
2022-09-19 23:42:35 +0300

    New Home Work Exception-closure

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,64 @@
+//Задание  №1
+
+function parseCount(number) {
+	if (isNaN(Number.parseInt(number))) {
+		throw new Error('Невалидное значение')
+
+	} return +number;
+}
+function validateCount(number) {
+
+
+
+
+	try {
+		return parseCount(number);
+
+
+	} catch (error) {
+		return error;
+	}
+
+}
+
+//Задание  №2
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if ((a + b) < c || (a + c) < b || (b + c) < a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+	getPerimeter() {
+		const perimeterTriangle = this.a + this.b + this.c;
+		return perimeterTriangle;
+	}
+	getArea() {
+		const p = (this.a + this.b + this.c) * 0.5; //Вычисление полупериметра
+		const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+		return +s;
+	}
+}
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (e) {
+		return {
+			getPerimeter() {
+				return "Ошибка! Треугольник не существует";
+			},
+			getArea() {
+				return "Ошибка! Треугольник не существует";
+			}
+		}
+	}
+}
+
+
+
+
+
+
